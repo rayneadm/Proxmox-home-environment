010126f1f25419a43fdf6063c5162892057567c8
rayne <rayne@mail.ru>
2024-02-28 04:11:18 +0300

    changes

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "registry.terraform.io/telmate/proxmox"
+      source = "registry.terraform.io/telmate/proxmox"
     }
   }
   required_version = ">= 0.14"
--- variables.tf
+++ variables.tf
@@ -17,11 +17,47 @@ variable "sshkeys" {
   description = "SSH keys for authentication"
   type        = string
 }
+
 variable "localip" {
   description = "Local IP address for SSH access"
-  type        = string 
+  type        = string
 }
+
 variable "localgw" {
   description = "Local gateway address for SSH access"
   type        = string
 }
+
+variable "instance_count" {
+  description = "The number of virtual machines to create"
+  type        = number
+  default     = 3
+}
+
+variable "ip_start" {
+  description = "The starting IP address for the virtual machines"
+  type        = string
+  default     = "192.168.1.80"
+}
+
+variable "gateway" {
+  description = "The gateway address"
+  type        = string
+  default     = "192.168.1.254"
+}
+
+variable "dns1" {
+  description = "The DNS server address"
+  type        = string
+  default     = "8.8.8.8"
+}
+variable "dns2" {
+  description = "The DNS server address"
+  type        = string
+  default     = "8.8.4.4"
+}
+
+
+
+
+
